bb98f4a4d622d953cf056ba20e2fce60338f2774
Professor <Professor@SN-388047.edu.fiesc.com.br>
2026-02-19 18:54:40 -0300

    aula 3

--- backend/utils/fileHandler.js
+++ backend/utils/fileHandler.js
@@ -3,13 +3,13 @@ const path = require('path')
 
 const dataPath = path.join(__dirname, '..', 'data', 'users.json');
 
-function saveUser(users){
-    fs.writeFileSync(dataPath, JSON.stringify(users, null, 2))
-}
-
 function readUsers(){
     const data = fs.readFileSync(dataPath, 'utf-8')
     return JSON.parse(data);
+}
+
+function saveUser(users){
+    fs.writeFileSync(dataPath, JSON.stringify(users, null, 2))
 }
 
 module.exports = {
